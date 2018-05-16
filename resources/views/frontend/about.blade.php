9e7b325fa2597d61f4bbe6a30631ca9de48915b0
khaledbelal Dot Net <khaledbelal@live.com>
2018-05-16 13:49:05 +0200

    Front-end - About page

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -11,15 +11,29 @@
 
 <div class="page-content back_to_up " > 
 	<div class="row row-fluid clearfix "> 
-		<div class="span6 animtt" data-gen="fadeUp"  style="opacity:0;">
-			<div class="def-block" >
-				<h4> إعتذار </h4><span class="liner"></span> 
-				<div class="news row-fluid animtt"> 
-					<div class="span12">
-						<h3 class="news-title"> الصفحة قيد التطوير  </h3>
-						<p>جاري العمل على اعداد الموقع بشكل كامل حتى يلائم رغباتكم.</p> 
-					</div><!-- span7 -->
-				</div><!-- news --> 
+		<div class="span12" >
+			<div class="def-block "  >
+				<h4> عن الموقع </h4><span class="liner"></span> 
+				<div class="news row-fluid ">  
+					<img class="four-radius " src="{{ URL('/images/tape.png') }}" alt="#" style="float:left;margin-right: 20px;margin-bottom: 10px;width: 40%;">
+					<h3 class="news-title">ما هي مقدمات الاشرطة؟</h3>
+					<p>من قبل هذا التطور التقني الكبير الذي حدث مؤخرا في القرن 21 كانت تسجل الدروس والمحاضرات الاسلامية على الاشرطة المدمجة (الكاسيت) ومن ثم بيعها وتداولها وقد كانت بعض الشركات والافراد يقومون بعمل اختصار للشريط الذي قد يحوى على ساعة او اكثر الى بضع دقائق تكون مقدمة للشريط لمعرفة بنود ومحتوى الشريط بشكل سريع.</p>  
+
+
+				 	<h3 class="news-title">ما قمنا به ..</h3>
+					<p>حاولنا قدر المستطاع العمل على تقطيع هذه المقدمات وجمعها في مكان واحد لما فيها من عبر ومواعظ سريعة وبشكل قد يؤتي ثماره باذن الله بشكل اسرع من سماع الشريط بشكل كامل.</p>   
+
+					<h3 class="news-title">هدفنا ..</h3>
+					<p>هدفنا هو جمع هذه المقدمات في مكان واحد وتقسيمها لعناصر ليسهل على المستمع التجوال بينها والاستماع اليها ومشاركتها بين الاصدقاء ليكون لنا ولكم ثوابا يوم القيامة باذن الله.</p>   
+					<h3 class="news-title">دورك معنا ..</h3>
+					<p>ليس فقط نحن من سيقوم بهذا العمل بل لك انت دور ان شاء الله في هذا ليكون لك ثوابه وجميع من عمل به الى يوم القيامة فيمكنك ارسال المقدمة لنا عبر البريد الالكتروني او المراسلة عبر الموقع - قريبا ان شاء الله سيكون هناك لوحة تحكم خاصة بك يمكنك من خلالها اضافة المقدمات مباشرة -</p>    
+				</div>   
+				<div class=" row-fluid clearfix"> 	 
+						<p class="news-title" style="text-align: center;margin-top: 20px;"><label style="color: #FF0078; cursor: default;">- شارك وانشر يكن في ميزان حسناتك بإذن الله - </label>روى عن أبي هريرة رضي الله عنه عن النبي صلى الله عليه وسلم أنه قال: <label style="color: #fff; cursor: default;">« من دعا إلى هدى كان له من الأجر مثل أجور من تبعه لا ينقص ذلك من أجورهم شيئا ومن دعا إلى ضلالة كان عليه من الإثم مثل آثام من تبعه لا ينقص ذلك من آثامهم شيئا »</label> (أخرجه مسلم في صحيحه). </p> 
+					</div>
+				<div class=" row-fluid clearfix" style="margin-top: 20px;">
+						<div class="sharethis-inline-share-buttons"></div> 
+					</div>
 			</div><!-- def block -->
 		</div><!-- span8 news --> 
 	</div><!-- row clearfix --> 
